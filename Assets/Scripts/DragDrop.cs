6dcd8032f18fac3d766ce14be9a553078865d1b7
SaitouRedEyes <zeca.saitou@gmail.com>
2016-05-03 14:32:58 -0300

    2D Joints example on Unity
    This is a basic example of the use of Hinge, Distance and Spring Joints
    on Unity.

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragDrop : MonoBehaviour
+{
+    private Vector3 screenPoint, offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+}
---
+++ Assets/Scripts/HingeJointSample.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HingeJointSample : MonoBehaviour
+{
+    private GameObject[] spawnPoints;
+    private GameObject ball, rota;
+    private SpriteRenderer ballSpriteRenderer;
+    private Rigidbody2D ballRigidbody;
+
+    public GameObject motorPlatform;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        ball = (GameObject)Instantiate(Resources.Load("Prefabs/Ball"), 
+                                       GetSpawnPoint(),
+                                       Quaternion.identity);
+
+        ballSpriteRenderer = ball.GetComponent<SpriteRenderer>();
+        ballRigidbody = ball.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ballSpriteRenderer.isVisible)
+        {
+            ballRigidbody.velocity = Vector3.zero;
+            ball.transform.position = GetSpawnPoint();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            JointMotor2D motor = motorPlatform.GetComponent<HingeJoint2D>().motor;
+            motor.motorSpeed *= -1;
+            motorPlatform.GetComponent<HingeJoint2D>().motor = motor;
+        }
+    }
+
+    private Vector3 GetSpawnPoint()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+    }
+
+}
